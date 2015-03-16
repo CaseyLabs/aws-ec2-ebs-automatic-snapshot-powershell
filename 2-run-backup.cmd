39113dcb70bb77cc8c8374c54eff2182fe861254
Casey Labs <sean@caseylabs.com>
2015-03-16 14:19:57 -0700

    Clarifying home directory instructions

--- 2-run-backup.cmd
+++ 2-run-backup.cmd
@@ -4,5 +4,8 @@
 # Because in Windows 2012, the Task Scheduler passes the Default User environment variables, 
 # and therefore can't get the admin user's AWS credentials.
 
+# NOTE: if you configured the AWS credentials under a user other than the Administrator account, you will need to set
+# the USERPROFILE below to match that user's home directory.
 set USERPROFILE=C:\Users\Administrator\
+
 powershell.exe -ExecutionPolicy Bypass -file "C:\aws\3-ebs-snapshot.ps1"
