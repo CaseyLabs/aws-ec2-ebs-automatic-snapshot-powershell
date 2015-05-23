50c623b6bcddefa8b135f7189e94f4bd6180bfa1
Casey Labs <sean@caseylabs.com>
2015-05-23 16:19:58 -0700

    Intermediary script cleanup.

--- 2-run-backup.cmd
+++ 2-run-backup.cmd
@@ -1,11 +1,15 @@
+#############################################################
+#
+# EBS Automatic Snapshot - Part #2: Intermeditary Script
+# By Casey Labs Inc.
+# Github repo: https://github.com/CaseyLabs/aws-ec2-ebs-automatic-snapshot-powershell
+#
+############################################################
+
 # Intermediary batch script - called by the Disk Shadow script.
 #
-# Why do we also have this separate batch script to call the EBS snapshot script? 
-# Because in Windows 2012, the Task Scheduler passes the Default User environment variables, 
-# and therefore can't get the admin user's AWS credentials.
+# NOTE: if you configured the AWS credentials under a Windows user other than the Administrator account, 
+# you will need to set the USERPROFILE below to match that Window user's profile directory.
 
-# NOTE: if you configured the AWS credentials under a user other than the Administrator account, you will need to set
-# the USERPROFILE below to match that user's home directory.
 set USERPROFILE=C:\Users\Administrator\
-
 powershell.exe -ExecutionPolicy Bypass -file "C:\aws\3-ebs-snapshot.ps1"
