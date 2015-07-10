faccba90c17d5da0ef7f70e4fad991372b61b3c4
jboeshart <jboeshart@users.noreply.github.com>
2015-07-10 14:30:37 -0500

    Add drive letter to snapshot description
    I've added some code sourced from [1] that adds the drive letter of the volume to the description of the snapshot in the format of servername-c:-backup-YYYY-MM-DD. This makes it a bit easier to identify a snapshot for a specific volume, especially if the server has multiple EBS volumes attached, even more so if they are all the same size. 
    
    I've tested this out in my AWS account and it works fine, but not sure what all validation is needed from your side. 
    
    [1] http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/ec2-windows-volumes.html

--- 3-ebs-snapshot.ps1
+++ 3-ebs-snapshot.ps1
@@ -52,7 +52,8 @@ function prereqcheck {
 # Snapshot all volumes attached to this instance.
 function snapshot_volumes {
 	foreach($volume_id in $volume_list)	{
-		$description="$hostname-backup-$today"
+		$letter = get_drive_letter($volume_id)
+		$description="$hostname-$letter-backup-$today"
 		$global:log_message = $global:log_message + "Volume ID is $volume_id" + $nl
     
 		# Take a snapshot of the current volume, and capture the resulting snapshot ID
@@ -84,6 +85,55 @@ function cleanup_snapshots {
 			}
 		}
 	}
+}
+
+function get_drive_letter($volId) {
+    # Much of this function was pulled from Powershell code in the AWS documentation at
+    # http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/ec2-windows-volumes.html
+    # Get the drive letter for the volume ID that was passed in
+
+    # Create a hash table that maps each device to a SCSI target
+    $Map = @{"0" = '/dev/sda1'} 
+    for($x = 1; $x -le 26; $x++) {$Map.add($x.ToString(), [String]::Format("xvd{0}",[char](97 + $x)))}
+    for($x = 78; $x -le 102; $x++) {$Map.add($x.ToString(), [String]::Format("xvdc{0}",[char](19 + $x)))}
+
+    #Get the volumes attached to this instance
+    $BlockDeviceMappings = (Get-EC2Instance -Region $region -Instance $instance_id).Instances.BlockDeviceMappings
+
+    $drives = Get-WmiObject -Class Win32_DiskDrive | % {
+        $Drive = $_
+        # Find the partitions for this drive
+        Get-WmiObject -Class Win32_DiskDriveToDiskPartition |  Where-Object {$_.Antecedent -eq $Drive.Path.Path} | %{
+            $D2P = $_
+            # Get details about each partition
+            $Partition = Get-WmiObject -Class Win32_DiskPartition |  Where-Object {$_.Path.Path -eq $D2P.Dependent}
+            # Find the drive that this partition is linked to
+            $Disk = Get-WmiObject -Class Win32_LogicalDiskToPartition | Where-Object {$_.Antecedent -in $D2P.Dependent} | %{ 
+                $L2P = $_
+                #Get the drive letter for this partition, if there is one
+                Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.Path.Path -in $L2P.Dependent}
+            }
+            $BlockDeviceMapping = $BlockDeviceMappings | Where-Object {$_.DeviceName -eq $Map[$Drive.SCSITargetId.ToString()]}
+           
+            # Display the information in a table
+            New-Object PSObject -Property @{
+                Device = $Map[$Drive.SCSITargetId.ToString()];
+                Disk = [Int]::Parse($Partition.Name.Split(",")[0].Replace("Disk #",""));
+                Boot = $Partition.BootPartition;
+                Partition = [Int]::Parse($Partition.Name.Split(",")[1].Replace(" Partition #",""));
+                SCSITarget = $Drive.SCSITargetId;
+                DriveLetter = If($Disk -eq $NULL) {"NA"} else {$Disk.DeviceID};
+                VolumeName = If($Disk -eq $NULL) {"NA"} else {$Disk.VolumeName};
+                VolumeId = If($BlockDeviceMapping -eq $NULL) {"NA"} else {$BlockDeviceMapping.Ebs.VolumeId}
+            }
+        }
+    }
+    foreach ($d in $drives) {
+        if ($volId -eq $d.VolumeId) {
+            $driveletter = $d.DriveLetter
+        }
+    }
+    return $driveletter
 }
 
 ## START COMMANDS
